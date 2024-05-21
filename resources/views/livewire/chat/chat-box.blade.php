e09c2d2da78c3b9e8e8017e1c5fa3953f708a754
evansmwenda <evansmwenda.em@gmail.com>
2024-05-21 20:47:38 +0300

    add chat box component  for conversation screen

---
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <header class="w-full sticky inset-x-0 flex pb-[3px] pt-[3px] top-0 z-10 bg-white border-b " >
+        <div class="flex w-full items-center px-2 lg:px-4 gap-2 md:gap-5">
+
+            <a class="shrink-0 lg:hidden" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                </svg>
+            </a>
+
+
+            <div class="shrink-0">
+                <x-avatar class="h-9 w-9 lg:w-11 lg:h-11" />
+            </div>
+            <h6 class="font-bold truncate"> emailaddress@gmail.com </h6>
+        </div>
+    </header>
+</div>
--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -5,11 +5,6 @@
             <div class="flex items-center gap-2">
                 <h5 class="font-extrabold text-2xl">Chats</h5>
             </div>
-            <button>
-                <svg class="w-7 h-7"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
-                </svg>
-            </button>
         </div>
     </header>
     <main class=" overflow-y-scroll overflow-hidden grow  h-full relative " style="contain:content">
